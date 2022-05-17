70e880955383cb142abe51ba76cec02f6ebd0d5f
KhunNooDev <noo_okok@hotmail.com>
2022-05-17 14:58:39 +0700

    add reactstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import { Routes, Route, Link, Outlet } from 'react-router-dom'
-
+import { useState } from 'react'
+import { Routes, Route, NavLink, Outlet } from 'react-router-dom'
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink as BSNavLink,
+} from 'reactstrap'
 import Home from './pages/home'
 import Time from './pages/time'
 import Form from './pages/form'
@@ -10,6 +19,7 @@ export default function App() {
     datapath is an array of objects with text and path properties for each link 
     in the header nav bar and the current page in the main content area (Outlet)
   */
+  const webName = 'Learn-React'
   const datapath = [
     { text: 'Home', path: '/', component: Home },
     { text: 'Time', path: '/time', component: Time },
@@ -19,7 +29,10 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route path={datapath[0].path} element={<Header datapath={datapath} />}>
+        <Route
+          path={datapath[0].path}
+          element={<Header webname={webName} datapath={datapath} />}
+        >
           {datapath.map((item) => (
             <Route
               key={item.text}
@@ -35,18 +48,59 @@ export default function App() {
 
 function Header(props) {
   const datapath = [...props.datapath]
+  const webName = props.webname
+  // openMenu is used to toggle the navbar collapse on small screens
+  const [openMenu, setOpenMenu] = useState(false)
+  const toggle = () => setOpenMenu(!openMenu)
   return (
     <>
-      <nav>
-        <ul>
-          {datapath.map((item) => (
-            <li key={item.text}>
-              <Link to={item.path}>{item.text}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <section>
+      <section className='container-fluid'>
+        <Navbar color='light' expand='md' light>
+          <NavbarBrand
+            to='/'
+            tag={NavLink}
+            style={{ textTransform: 'uppercase' }}
+          >
+            {webName}
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={openMenu} navbar>
+            <Nav className='me-auto' navbar>
+              {datapath.map((item) => (
+                <NavItem key={item.text}>
+                  <BSNavLink to={item.path} tag={NavLink}>
+                    {item.text}
+                  </BSNavLink>
+                </NavItem>
+              ))}
+              {/* <UncontrolledDropdown inNavbar nav>
+                <DropdownToggle caret nav>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown> */}
+            </Nav>
+            <Nav navbar>
+              <NavItem>
+                <BSNavLink to='/' tag={NavLink}>
+                  Sign In
+                </BSNavLink>
+              </NavItem>
+              <NavItem>
+                <BSNavLink to='/' tag={NavLink}>
+                  Sign Up
+                </BSNavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </section>
+      <section className='container'>
         <Outlet />
       </section>
     </>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
